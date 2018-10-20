11bfb27f3d41ca1fe3676f33871a4fe4b6186d78
Patryk <pat049b@gmail.com>
2018-10-20 19:37:35 +0200

    Add numpy, fixing bugs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import datetime
 import random
+import math
 
 class Cell:
 
@@ -9,10 +10,21 @@ class Cell:
 		self.id = ident
 		self.timestamp = timestamp
 		self.state = state
+		self.previous_timestamp = timestamp
+		self.previous_state = state
+
 	
 	def find_id(self):
 		x , y = self.id.split(':')
 		return int(x), int(y)
+
+
+	def change_state(self, timestamp, state):
+		self.previous_state = self.state
+		self.previous_timestamp = self.timestamp
+		self.timestamp = timestamp
+		self.state = state
+
 
 
 class CA_space:
@@ -26,58 +38,52 @@ class CA_space:
 
 	def generate_grains(self, cells):
 		for cell_num in range(cells):
-			sample_cell = random.choice(self.space)
-			sample_cell[0].state = cell_num
+			sample_row = random.sample(self.space, 1)
+			sample_row = sample_row[0]
+			sample_cell = random.sample(sample_row, 1)
+			sample_cell = sample_cell[0]
+			while sample_cell.state != 0:
+				sample_row = random.sample(self.space, 1)
+				sample_row = sample_row[0]
+				sample_cell = random.sample(sample_row, 1)
+				sample_cell = sample_cell[0]
+			sample_cell.state = cell_num
 
 
 	def find_neigh(self, cell):
 		x , y = cell.find_id()
-		try:
-			if x > 0 and y > 0 and x < space.length and y < space[0].length:
-				return [self.space[x-1][y-1],self.space[x-1][y], self.space[x-1][y+1], self.space[x][y-1], self.space[x][y+1] , self.space[x+1][y-1], self.space[x+1][y], self.space[x+1][y+1]]
-			elif x == 0:
-				if y == 0:
-					return [self.space[x+1][y+1], self.space[x][y+1], self.space[x+1][y+1]]
-				elif y < self.space[0].length:
-					return [self.space[x][y-1],self.space[x][y+1], self.space[x+1][y],self.space[x+1][y-1], self.space[x+1][y+1]]
-				else:
-					return [self.space[x-1][y-1], self.space[x-1][y]. self.space[x][y-1]]
-			elif x == self.space.length:
-				if y == 0:
-					return [self.space[x -1][y], self.space[x-1][y+1], self.space[x][y+1]]
-				elif y < self.space[0].length:
-					return [self.space[x-1][y-1],self.space[x-1][y], self.space[x-1][y+1],self.space[x][y-1],self.space[x][y+1]]
-				else:
-					return [self.space[x-1][y-1],self.space[x-1][y],self.space[x][y-1]]
-			elif y == 0:
-				pass
-			else:
-				pass
+		neighbours = []
+		for row in self.space:
+			for c in  row:
+				i , j = cell.find_id()
+				if math.fabs(x - i) <= 1 and math.fabs(y -j) <= 1:
+					neighbours.append(c)
+		return neighbours
 
-		except IndexError:
-			return []
 
 	def build_grains(self):
+		time = datetime.datetime.now()
 		for row in self.space:
 			for cell in row:
 				if cell.state != 0 :
 					continue
 				else:
 					neighbours = self.find_neigh(cell)
-					if neighbours is []:
-						return "Error."
-					grains = [0 for i in range(1,self.grains+1)]
+					grains = [0 for i in range(self.grains)]
 					for i in range(1,self.grains+1):
 						for neighbour in neighbours:
-							if neighbour.state is i:
+							if neighbour.previous_state == i:
+
 								grains[i] = grains[i] + 1
+					new_grain = 0
+					for i in range(self.grains):
+						if grains[i] >= new_grain:
+							new_grain = i
+					cell.change_state(time, new_grain)
 
 
 
 
 
-space = CA_space(200,200,2)
-#space.build_grains()
-for row in space.space:
-	for cell in row:
-		print(cell.state)
+CA = CA_space(20,20,50)
+CA.build_grains()
